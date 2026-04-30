1a27abc04ccfe22705a150178a4610756cb236f2
Bruno Kemel <br.kemel@gmail.com>
2026-04-29 21:09:25 -0300

    tirando compras

--- data/race-data.ts
+++ data/race-data.ts
@@ -1,72 +1,72 @@
 import type { RaceKit, Sponsor } from '@/types/race'
 
 export const raceKits: RaceKit[] = [
-  {
-    id: '1km-lote1',
-    raceName: '1kmzinho Kids',
-    distance: '1km',
-    price: 49.90,
-    lot: 1,
-    availableSlots: 100,
-    soldSlots: 25,
-    description: 'Corrida especial para crianças de 5 a 10 anos',
-    img: "/banner.png"
-  },
-  {
-    id: '3km-lote1',
-    raceName: 'Desafio 3K',
-    distance: '3km',
-    price: 79.90,
-    lot: 1,
-    availableSlots: 200,
-    soldSlots: 150,
-    description: 'Ideal para iniciantes e caminhada',
-    img: "/banner2.png"
-  },
-  {
-    id: '5km-lote1',
-    raceName: 'Circuito 5K',
-    distance: '5km',
-    price: 99.90,
-    lot: 1,
-    availableSlots: 300,
-    soldSlots: 280,
-    description: 'Percurso intermediário para todos os níveis',
-    img: "/banner3.png"
-  },
-  {
-    id: '10km-lote1',
-    raceName: 'Corrida manos e manas',
-    distance: '5km',
-    price: 129.90,
-    lot: 1,
-    availableSlots: 150,
-    soldSlots: 145,
-    description: 'Corrida manos e manas, venha fazer',
-    img: "/bannerManosManas.png"
-  },
-  {
-    id: '21km-lote1',
-    raceName: 'Meia Maratona',
-    distance: '21km',
-    price: 189.90,
-    lot: 1,
-    availableSlots: 100,
-    soldSlots: 60,
-    description: 'O desafio dos verdadeiros atletas',
-    img: "/banner.png"
-  },
-  {
-    id: 'teste-compra-lote1',
-    raceName: 'Teste de Compra',
-    distance: '5km',
-    price: 1,
-    lot: 1,
-    availableSlots: 50,
-    soldSlots: 0,
-    description: 'Card de teste para validar compras no Mercado Pago',
-    img: "/bannerManosManas.png"
-  }
+  // {
+  //   id: '1km-lote1',
+  //   raceName: '1kmzinho Kids',
+  //   distance: '1km',
+  //   price: 49.90,
+  //   lot: 1,
+  //   availableSlots: 100,
+  //   soldSlots: 25,
+  //   description: 'Corrida especial para crianças de 5 a 10 anos',
+  //   img: "/banner.png"
+  // },
+  // {
+  //   id: '3km-lote1',
+  //   raceName: 'Desafio 3K',
+  //   distance: '3km',
+  //   price: 79.90,
+  //   lot: 1,
+  //   availableSlots: 200,
+  //   soldSlots: 150,
+  //   description: 'Ideal para iniciantes e caminhada',
+  //   img: "/banner2.png"
+  // },
+  // {
+  //   id: '5km-lote1',
+  //   raceName: 'Circuito 5K',
+  //   distance: '5km',
+  //   price: 99.90,
+  //   lot: 1,
+  //   availableSlots: 300,
+  //   soldSlots: 280,
+  //   description: 'Percurso intermediário para todos os níveis',
+  //   img: "/banner3.png"
+  // },
+  // {
+  //   id: '10km-lote1',
+  //   raceName: 'Corrida manos e manas',
+  //   distance: '5km',
+  //   price: 129.90,
+  //   lot: 1,
+  //   availableSlots: 150,
+  //   soldSlots: 145,
+  //   description: 'Corrida manos e manas, venha fazer',
+  //   img: "/bannerManosManas.png"
+  // },
+  // {
+  //   id: '21km-lote1',
+  //   raceName: 'Meia Maratona',
+  //   distance: '21km',
+  //   price: 189.90,
+  //   lot: 1,
+  //   availableSlots: 100,
+  //   soldSlots: 60,
+  //   description: 'O desafio dos verdadeiros atletas',
+  //   img: "/banner.png"
+  // },
+  // {
+  //   id: 'teste-compra-lote1',
+  //   raceName: 'Teste de Compra',
+  //   distance: '5km',
+  //   price: 1,
+  //   lot: 1,
+  //   availableSlots: 50,
+  //   soldSlots: 0,
+  //   description: 'Card de teste para validar compras no Mercado Pago',
+  //   img: "/bannerManosManas.png"
+  // }
 ]
 
 export const sponsors: Sponsor[] = [
